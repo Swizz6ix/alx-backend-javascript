eb8eaca86279533dadb75b88ff4c6fee8dd11d24
PlayBoi Baby <ferdinandcharles6@gmail.com>
2023-09-30 11:59:43 +0100

    ES6-basics

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,4 +1,4 @@
-// Rewrite the function appendToEachArrayValue to use ES6’s 
+// Rewrite the function appendToEachArrayValue to use ES6’s
 // for...of operator. And don’t forget that var is not ES6-friendly.
 export default function appendToEachArrayValue(array, appendString) {
   const value = [];
--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,4 +1,4 @@
-//Modify the following function’s budget object to simply use the keyname instead.
+// Modify the following function’s budget object to simply use the keyname instead.
 export default function getBudgetObject(income, gdp, capita) {
   const budget = {
     income,

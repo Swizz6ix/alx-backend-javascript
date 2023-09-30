b3c32f8cbac173b9b88d84498ee4e888c3640ba8
PlayBoi Baby <ferdinandcharles6@gmail.com>
2023-09-30 22:03:22 +0100

    ES6-classes

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,30 +1,30 @@
 export default class Airport {
-    constructor (name, code) {
-        this._name = name,
-        this._code = code
-    };
+  constructor (name, code) {
+    this._name = name,
+    this._code = code
+  };
 
-    get name () {
-        return this._name;
-    };
+  get name () {
+    return this._name;
+  };
 
-    set name (name) {
-        if (typeof name !== 'string')
-            throw new TypeError('name must be a string');
-        return this._name = name;
-    }
+  set name (name) {
+    if (typeof name !== 'string')
+      throw new TypeError('name must be a string');
+    return this._name = name;
+  }
 
-    get code () {
-        return this._code;
-    };
+  get code () {
+    return this._code;
+  };
 
-    set code (code) {
-        if (typeof code !== 'string')
-            throw new TypeError('code must be a string');
-        return this._code = code;
-    };
+  set code (code) {
+    if (typeof code !== 'string')
+      throw new TypeError('code must be a string');
+    return this._code = code;
+  };
 
-    get [Symbol.toStringTag]() {
-        return this._code;
-    }
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
 }

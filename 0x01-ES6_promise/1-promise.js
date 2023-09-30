1cb5141c8b3d6d0f0d074769572bacc3f5c3d94e
PlayBoi Baby <ferdinandcharles6@gmail.com>
2023-09-30 14:10:37 +0100

    ES6-Promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,10 @@
 function getFullResponseFromAPI(success) {
   return (new Promise((resolve, reject) => {
-    success? resolve({
-        status: 200,
-        body: 'Success',
-      })
-    : reject(new Error('The fake API is not working currently'));
+    success ? resolve({
+      status: 200,
+      body: 'Success',
+    })
+      : reject(new Error('The fake API is not working currently'));
   })
   );
 }
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,16 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
-
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-  .then(([signupRes, uploadRes]) => [
-    {
-      status: signupRes.status,
-      value: signupRes.status === 'fulfilled'? signupRes.value : signupRes.reason
-    },
-    {
-      status: uploadRes.status,
-      value: uploadRes.status === 'fulfilled'? uploadRes.value : uploadRes.reason.message
-    },
-  ]);
+    .then(([signupRes, uploadRes]) => [
+      {
+        status: signupRes.status,
+        value: signupRes.status === 'fulfilled' ? signupRes.value : signupRes.reason
+      },
+      {
+        status: uploadRes.status,
+        value: uploadRes.status === 'fulfilled' ? uploadRes.value : uploadRes.reason.message
+      },
+    ]);
 }

9ecfc68a73b087359116d46ec860e8b097c8f25d
PlayBoi Baby <ferdinandcharles6@gmail.com>
2023-09-30 22:50:01 +0100

    ES6-classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,48 +1,48 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name,
-    this._length = length,
-    this._students = students
-  };
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
   get name() {
-    return this._name
+    return this._name;
   };
 
   set name(name) {
     if (typeof name !== 'string') {
-      return new TypeError('Name must be a string')
+      return new TypeError('Name must be a string');
     } else {
-      return this._name = name
-    };
-  };
+      this._name = name;
+    }
+  }
 
   get length() {
-    return this._length
-  };
+    return this._length;
+  }
 
   set length(length) {
     if (typeof length !== 'number') {
-      throw new TypeError(`Length must be a number`)
+      throw new TypeError(`Length must be a number`);
     } else {
       return this._length = length;
-    };
-  };
+    }
+  }
 
   get students() {
-    return this._students
-  };
+    return this._students;
+  }
 
   set students(students) {
     if (!Array.isArray(students)) {
-      throw new TypeError('Students must be an array')
+      throw new TypeError('Students must be an array');
     } 
     
     for (let student of students) {
       if (typeof student !== "string")
-        throw new TypeError('Student must be a string')
+        throw new TypeError('Student must be a string');
     }
     
-    return this._students = students;
-  };
+    this._students = students;
+  }
 }

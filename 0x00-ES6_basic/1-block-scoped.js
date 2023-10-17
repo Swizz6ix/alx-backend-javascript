8a277b5739b16cf4b8a2c74a6654984015226d32
PlayBoi Baby <ferdinandcharles6@gmail.com>
2023-10-18 00:32:43 +0100

    fix issues

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,10 @@
-// Given what you’ve read about var and hoisting, modify the variables
-// inside the function taskBlock so that the variables aren’t overwritten
-// inside the conditional block.
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];

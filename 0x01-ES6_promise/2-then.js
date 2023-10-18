cf68faa979dc9a32ab6299f2704804ae6aa13b07
PlayBoi Baby <ferdinandcharles6@gmail.com>
2023-10-18 02:37:15 +0100

    fix issues

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,8 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({
-        status: 200,
-        body: 'success',
+      status: 200,
+      body: 'success',
     }))
     .catch((error) => new Error(error))
     .finally(() => console.log('Got a response from the API'));
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,8 +1,15 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  try { queue.push(mathFunction()); }
-  catch (error) { queue.push(String(error)); }
-  finally { queue.push('Guardrail was processed'); }
+
+  try {
+    queue.push(mathFunction());
+  }
+  catch (error) {
+    queue.push(String(error));
+  }
+  finally { 
+    queue.push('Guardrail was processed');
+  }
 
   return queue;
 }

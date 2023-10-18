eaf50946ef1ecadacc428771ba3a1f7f204e2d35
PlayBoi Baby <ferdinandcharles6@gmail.com>
2023-10-18 02:31:15 +0100

    fix issues

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,10 +1,8 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  try {
-    queue.push(mathFunction());
-  }
-  catch (error) {queue.push(String(error))}
-  finally {queue.push('Guardrail was processed')}
+  try { queue.push(mathFunction()); }
+  catch (error) { queue.push(String(error)); }
+  finally { queue.push('Guardrail was processed'); }
 
   return queue;
 }
